9dfcfb916cdb7e5ec4db2d8f2036262a0771893e
root <root@ARM-64.localdomain>
2021-08-06 13:45:32 +0300

    rename startup bash script

---
+++ start_docker_containers.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source aws_credentials
+PS3='Choose Docker Image: '
+options=("Ansible" "Terraform" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Ansible")
+            docker build -t ansible:latest ansible/ --build-arg AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} --build-arg AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+            docker run -v $PWD/ansible/ansible-conf/:/volumes/ansible-conf -v $PWD/ansible/ssh-key/:/volumes/ssh-key -it ansible:latest
+            break
+            ;;
+        "Terraform")
+            docker build -t terraform:latest terraform/ --build-arg TER_VER=`curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | grep tag_name | cut -d: -f2 | tr -d \"\,\v | awk '{$1=$1};1'` --build-arg AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} --build-arg AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+            docker run -v $PWD/terraform/terraform-conf/:/volumes/terraform-conf -v $PWD/terraform/ssh-key/:/volumes/ssh-key -it terraform:latest
+            break
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
