87d97965166a6f47643f4de417c2f514a2a6f314
root <root@DESKTOP-PV4H5Q0.localdomain>
2021-08-08 17:01:40 +0300

    fix install terraform from source and improved script

--- set_aws_credentials.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-read -p "Type your AWS_ACCESS_KEY_ID: "
-echo "AWS_ACCESS_KEY_ID=$REPLY" > aws-credentials
-
-read -p "Type your AWS_SECRET_ACCESS_KEY: "
-echo "AWS_SECRET_ACCESS_KEY=$REPLY" >> aws_credentials
-
-echo "Succefully saved your AWS Credentials!"
--- start_docker_containers.sh
+++ start_docker_containers.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-source aws_credentials
+FILE=aws_credentials
+if [ ! -f "$FILE" ]; then
+    echo -e "\033[1mYOU NEED TO SET YOUR AWS CREDENTIALS!\033[0m"
+fi
 PS3='Choose Docker Image: '
-options=("Ansible" "Terraform" "Quit")
+options=("Ansible" "Terraform" "Set AWS Credentials" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
         "Ansible")
+            source aws_credentials 2>/dev/null
             docker build -t ansible:latest ansible/ --build-arg AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} --build-arg AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
             docker run -v $PWD/ansible/ansible-conf/:/volumes/ansible-conf -v $PWD/ansible/ssh-key/:/volumes/ssh-key -it ansible:latest
             break
             ;;
         "Terraform")
-            docker build -t terraform:latest terraform/ --build-arg TER_VER=`curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | grep tag_name | cut -d: -f2 | tr -d \"\,\v | awk '{$1=$1};1'` --build-arg AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} --build-arg AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+            source aws_credentials 2>/dev/null
+            docker build -t terraform:latest terraform/ --build-arg AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} --build-arg AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
             docker run -v $PWD/terraform/terraform-conf/:/volumes/terraform-conf -v $PWD/terraform/ssh-key/:/volumes/ssh-key -it terraform:latest
+            break
+            ;;
+        "Set AWS Credentials")
+            read -p "Type your AWS_ACCESS_KEY_ID: "
+            echo "AWS_ACCESS_KEY_ID=$REPLY" > aws_credentials
+            read -p "Type your AWS_SECRET_ACCESS_KEY: "
+            echo "AWS_SECRET_ACCESS_KEY=$REPLY" >> aws_credentials
+            echo -e "\033[1mSuccefully saved your AWS Credentials!\033[0m"
+            ./start_docker_containers.sh
             break
             ;;
         "Quit")
